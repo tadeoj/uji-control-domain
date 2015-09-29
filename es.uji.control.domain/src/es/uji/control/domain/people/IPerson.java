5e259a7c1cc0262495981ff2257d8682d1ca9953
tjulian <tjulian@ceracloud.es>
2015-09-29 09:29:23 +0200

    Se estan construyendo los builders de los objetos de dominio.

--- es.uji.control.domain/src/es/uji/control/domain/people/IPerson.java
+++ es.uji.control.domain/src/es/uji/control/domain/people/IPerson.java
@@ -5,8 +5,8 @@ import java.util.List;
 public interface IPerson {
 	public IPersonIdentifier getId();
 	public String getName();
-	public String getLastName1();
-	public String getLastName2();
+	public String getFirstLastName();
+	public String getSecondLastName();
 	public String getIdentification();
 	public List<ILinkage>getLinkages();
 	public List<IAccreditation> getAccreditations();
