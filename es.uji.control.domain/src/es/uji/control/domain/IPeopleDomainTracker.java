f11590c99cc6540d26d1c76139b6537cc04959b0
padilla <padilla@uji.es>
2015-10-08 12:59:45 +0200

    Nueva propuesta de Dominio de control.

--- es.uji.control.domain/src/es/uji/control/domain/IPeopleDomainTracker.java
+++ es.uji.control.domain/src/es/uji/control/domain/IPeopleDomainTracker.java
@@ -1,6 +1,6 @@
 package es.uji.control.domain;
 
 public interface IPeopleDomainTracker {
-	abstract void opennedConnectorionFactory(IPeopleDomainConnectionFactory factory);
+	abstract void availableConnectorionFactory(IPeopleDomainConnectionFactory factory);
 	abstract void close();
 }
