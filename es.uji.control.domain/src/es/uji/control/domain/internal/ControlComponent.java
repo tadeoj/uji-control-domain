1471b2e310a315b2be2adddde1c8bb262cf15b80
padilla <padilla@uji.es>
2015-10-14 14:10:11 +0200

    Nueva propuesta de Dominio de control.

--- es.uji.control.domain/src/es/uji/control/domain/internal/ControlComponent.java
+++ es.uji.control.domain/src/es/uji/control/domain/internal/ControlComponent.java
@@ -1,6 +1,17 @@
 package es.uji.control.domain.internal;
 
-public class ControlComponent {
+import es.uji.control.domain.IControlConnectionFactorySelector;
 
+@Component(name="connectionfactory", immediate=true)
+public class ControlComponent implements IControlConnectionFactorySelector {
+
+	@Override
+	public String getCurrentFactory() {
+		return null;
+	}
+
+	@Override
+	public void setCurrentFactory() {
+	}
 
 }
